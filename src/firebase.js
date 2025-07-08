da37225a228caac8703f7ac680f012673ec63917
duncan-morton <duncan@zear.co.uk>
2025-07-08 16:44:44 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   // Firebase will give you these values
-  apiKey: "your-api-key",
-  authDomain: "your-auth-domain",
-  projectId: "your-project-id",
-  storageBucket: "your-storage-bucket",
-  messagingSenderId: "your-sender-id",
-  appId: "your-app-id"
+  apiKey: "AIzaSyBrb-Vo_4r-3NfXlUq-D7X5RUi-0Mfin0s",
+  authDomain: "wordle-nerdles.firebaseapp.com",
+  projectId: "wordle-nerdles",
+  storageBucket: "wordle-nerdles.firebasestorage.app",
+  messagingSenderId: "915853501327",
+  appId: "1:915853501327:web:33c0c54e421e88874921fa"
 };
 
 const app = initializeApp(firebaseConfig);
